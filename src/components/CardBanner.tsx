975f60b71c3b606d914893d87e009c965bbca164
unknown <danmaggot2@gmail.com>
2024-06-11 23:24:32 -0300

    s

--- src/components/CardBanner.tsx
+++ src/components/CardBanner.tsx
@@ -7,7 +7,7 @@ const CardBanner = ({data}: idata) => {
   return (
     <aside className='rounded-md flex items-center justify-between w-[80%] m-auto text-[1.1em]'>
         <div className='bg-[#597C24] w-full h-[5.2em] flex items-center justify-center rounded-l-md p-8'>
-            <p>{data.info}</p>
+            <p>{data.info} a</p>
         </div>
         <div className='bg-white w-[15em] h-[5.2em] text-[#6C8C3B] font-bold flex items-center justify-center rounded-r-md'>
             <button>SAIBA MAIS</button>
