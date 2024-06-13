d860553dac4741c6541fd3de14470efb3762f09a
unknown <danmaggot2@gmail.com>
2024-06-12 22:58:45 -0300

    navlink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import logo from '/logo.png'
 
 const Header = () => {
   return (
-    <header className='flex items-center justify-between p-4 px-20 fixed z-10 w-full bg-[#0D0D0D] top-0 border-b-2 border-b-lime-400'>
+    <header className='flex items-center justify-between p-4 px-20 fixed z-10 w-full bg-[#0D0D0D] top-0 border-b-2 border-b-lime-400 links'>
         <div className='flex items-center gap-2'>
             <img src={logo} alt="" className='w-8' />
             <h2 className='text-2xl font-medium tracking-wider font-Rubik'>WIKI<span className='text-lime-400'>MOEDA</span></h2>
@@ -12,10 +12,10 @@ const Header = () => {
 
         <nav>
             <ul className='grid grid-flow-col gap-x-2'>
-                <Link to={'/'} className='cursor-pointer px-2 flex hover:text-lime-400 w-30'><p>Inicio</p></Link>
-                <Link  to={'/'} className='cursor-pointer px-2 flex hover:text-lime-400 w-30'><p>Cotação</p></Link>
-                <Link   to={'/'}className='cursor-pointer px-2  hover:text-lime-400 w-30'>WIKI</Link>
-                <Link  to={'/noticias'} className='cursor-pointer px-2  hover:text-lime-400 w-30 text-center'>Noticias</Link>
+                <NavLink to={'/'} className='cursor-pointer px-2 flex hover:text-lime-400 w-30'><p>Inicio</p></NavLink>
+                <NavLink  to={'/cotacao'} className='cursor-pointer px-2 flex hover:text-lime-400 w-30'><p>Cotação</p></NavLink>
+                <NavLink   to={'/wiki'}className='cursor-pointer px-2  hover:text-lime-400 w-30'>WIKI</NavLink>
+                <NavLink  to={'/noticias'} className='cursor-pointer px-2  hover:text-lime-400 w-30 text-center'>Noticias</NavLink>
             </ul>
         </nav>
     </header>
