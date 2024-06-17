9bee0d9d00956a67c15f76d05bf866d8ed1c7013
unknown <danmaggot2@gmail.com>
2024-06-16 22:42:28 -0300

    path

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "${path.resolve(__dirname, 'src/styles/global.scss')}";`
+        additionalData: `@import "./path/to/aos.css";`
       }
     }
   }
