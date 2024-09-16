944dbe8ad5e4cf0d3e0f15222cd26cf3aba18a7d
unknown <danmaggot2@gmail.com>
2024-09-16 14:46:27 -0300

    :lipstick: feat: melhoria de estilização na pagina do conversor de moeda

--- src/Pages/Conversor/Conversor.tsx
+++ src/Pages/Conversor/Conversor.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { useFetch } from "../../Hooks/useFetch";
 import Loading from "../../components/Loading";
+import { FaCoins } from "react-icons/fa6";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
 const Conversor = () => {
-
   useEffect(() => {
     Aos.init();
   }, []);
@@ -19,12 +19,7 @@ const Conversor = () => {
     return convert.format(num);
   };
 
-  const query: string[] = [
-    "BRL",
-    "USD",
-    "EUR",
-    "AUD",
-  ];
+  const query: string[] = ["BRL", "USD", "EUR", "AUD"];
 
   const [oneSymbol, SetoneSymbol] = useState("USD");
   const [twoSymbol, SettwoSymbol] = useState("BRL");
@@ -39,9 +34,12 @@ const Conversor = () => {
   }
 
   return (
-    <section className="flex flex-col  items-center justify-center mb-40 gap-4"  data-aos="fade-right"
-    data-aos-duration="1000">
-      <section className="max-w-md max-auto flex flex-col gap-2 justify-center items-center ">
+    <section
+      className="flex flex-col items-center justify-center mb-40 gap-4"
+      data-aos="fade-right"
+      data-aos-duration="1000"
+    >
+      <section className=" flex flex-col gap-2 justify-center items-center ">
         <h2>Conversor de moedas</h2>
         <div className="flex flex-col gap-4">
           <div className="flex items-center justify-center px-2 rounded-s-md gap-2 relative">
@@ -55,12 +53,7 @@ const Conversor = () => {
                 Escolha a moeda
               </option>
               {query.map((coin) => (
-                  <>
-                  {coin != twoSymbol && 
-                    <option value={coin}>{coin}</option>
-                  }
-                  
-                </>
+                <>{coin != twoSymbol && <option value={coin}>{coin}</option>}</>
               ))}
             </select>
           </div>
@@ -76,31 +69,37 @@ const Conversor = () => {
               </option>
               {query.map((coin) => (
                 <>
-                {console.log(coin)}
-                  {coin != oneSymbol && 
-                    <option value={coin}>{coin}</option>
-                  }
-                  
+                  {coin != oneSymbol && <option value={coin}>{coin}</option>}
                 </>
               ))}
             </select>
           </div>
-          <input
-            type="number"
-            className="bg-gray-600 rounded-s-sm p-1 "
-            onChange={(e) => SetCalCoin(parseInt(e.target.value))}
-            value={calCoin}
-            min="1"
-          />
+          <label htmlFor="number" className="relative">
+            <span className="absolute top-0 left-0 translate-x-[50%] translate-y-[50%]
+             after:h-8 after:w-[.1px] after:bg-gray-500 after:absolute after:-top-2 after:-right-3
+            ">
+              <FaCoins />
+            </span>
+            <input
+              type="number"
+              className="bg-gray-600 rounded-md p-1 indent-10 outline-none shadow-none focus:outline-lime-400"
+              onChange={(e) => SetCalCoin(parseInt(e.target.value))}
+              placeholder="digite o valor"
+              value={calCoin}
+              min="1"
+              name="number"
+            />
+          </label>
         </div>
       </section>
-      <aside>
-        <h2>
-          {" "}
-          Resultado é{" "}
+        <div className="text-white ">
+          <h3>{data && Object.values(data).map((coin) => coin.name)}</h3>
+        </div>
+      <aside className="border-2 p-2 rounded-lg bg-lime-600 min-w-40 text-center">
+        <p>
           {data &&
-            Object.values(data).map((e) => currency(e.bid * calCoin, e.code))}
-        </h2>
+            Object.values(data).map((coin) => currency(coin.bid * calCoin, coin.code))}
+        </p>
       </aside>
     </section>
   );
